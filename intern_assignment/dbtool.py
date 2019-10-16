c91995e7b43e0b1003e6020d9e3d37db38364734
Matthew Epler <mepler@flashpoint-intel.com>
2019-10-16 09:31:45 -0400

    open_file

--- intern_assignment/dbtool.py
+++ intern_assignment/dbtool.py
@@ -1,2 +1,17 @@
-def hello():
-    return 'hello'
+from pathlib import Path
+
+def open_file(path:str) -> list:
+    """Returns file contents as list of dicts."""
+
+
+    file_to_open = Path(path)
+    rows = None
+
+    with open(file_to_open) as f:
+        rows = f.readlines()
+
+    if not rows: return None
+
+    rows_list = [r.split(',') for r in rows]
+        
+    return rows_list
--- tests/test_dbtool.py
+++ tests/test_dbtool.py
@@ -1,7 +1,18 @@
+import os
 import pytest
 
 import intern_assignment.dbtool as dbtool
 
+
 class TestDbtool():
-    def test_hello(self):
-        assert dbtool.hello() == 'hello'
+
+    def test_open_file(self, tmpdir):
+        # file fixture 
+        csv_fixture = tmpdir.mkdir('tmp').join('test.csv')
+        csv_fixture.write('header1,header2\nrow1a,row1b')
+
+        result = dbtool.open_file(csv_fixture)
+
+        assert result[0][0] == 'header1'
+        assert result[1][1] == 'row1b'
+
